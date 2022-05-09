8d0a8ff1c79887f8a3fbcb427db7a770865518a2
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2022-05-09 20:12:49 +0200

    Create Planning.java

---
+++ src/main/java/projet_web2/Planning.java
@@ -0,0 +1,25 @@
+package projet_web2;
+
+import java.util.ArrayList;
+
+public class Planning {
+	private Utilisateur u;
+	private ArrayList<Creneau> edt;
+	public Planning(Utilisateur user) {
+		super();
+		this.u = user;
+		this.edt = new ArrayList<Creneau>();
+	}
+	public Utilisateur getU() {
+		return u;
+	}
+	public void setU(User user) {
+		this.u = user;
+	}
+	public ArrayList<Creneau> getEdt() {
+		return edt;
+	}
+	public void addCreneau(Creneau cre) {
+		edt.add(cre);
+	}
+}
