92cda7a464777823696324ba1dab66267dc01bca
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2022-05-09 19:51:58 +0200

    Create dessin.js

---
+++ src/main/webapp/script/dessin.js
@@ -0,0 +1,13 @@
+function start() {
+  d=document.getElementById("zone_dessin");
+  d.addEventListener("mousedown", mouseDown);
+  d.addEventListener("mouseup", mouseUp);
+}
+
+function mouseDown() {
+  
+}
+
+function mouseUp() {
+  
+}
