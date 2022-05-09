dd63af0ca22d15349f505de904f887a2bf626f59
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2022-05-09 19:55:35 +0200

    Update dessin.js

--- src/main/webapp/script/dessin.js
+++ src/main/webapp/script/dessin.js
@@ -1,13 +1,14 @@
+d=document.getElementById("zone_dessin");
+
 function start() {
-  d=document.getElementById("zone_dessin");
   d.addEventListener("mousedown", mouseDown);
   d.addEventListener("mouseup", mouseUp);
 }
 
 function mouseDown() {
-  
+  d.innerHTML = "down";
 }
 
 function mouseUp() {
-  
+  d.innerHTML = "up";
 }
